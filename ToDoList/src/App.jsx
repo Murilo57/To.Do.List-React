981a93bca7048da78c4b649ae0d6a349b9e35e8c
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-07-21 10:17:22 -0300

    Finished project
    Finished functions of search and filter

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Search from "./components/Search";
 import All from "./components/All";
 import TodoForm from './components/TodoForm';
+import Filter from './components/Filter';
 
 function App() {
   const [alls, setAll] = useState([
@@ -19,6 +20,11 @@ function App() {
     text: "To study React", category: "Studys", isCompleted: false,
   }
 ])
+
+  const[search, setSearch] = useState("")
+
+  const [filter, setFilter] = useState("All")
+  const [sort, setSort] = useState("Asc")
 
   const addAll = (text, category) => {
     const newAlls = [
@@ -50,10 +56,29 @@ function App() {
   return (
     <div className="app">
     <h1>To-Do List</h1>
-    <Search/>
-      <div className="todo-list">
-        {alls.map((all) => (
-          <All key={all.id} all={all} removeTodo={removeTodo} completeTodo={completeTodo}/>
+    <Search search={search} setSearch={setSearch}/>
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/> 
+    <div className="todo-list">
+        {alls
+        .filter((all) => 
+        filter === "All" 
+        ? true 
+        : filter === "Completed" 
+        ? all.isCompleted 
+        : !all.isCompleted
+        )
+        .filter((all) => 
+        all.text.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => 
+        sort === "Asc" 
+        ? a.text.localeCompare(b.text) 
+        : b.text.localeCompare(a.text))
+        .map((all) => (
+          <All key={all.id}
+           all={all}
+           removeTodo={removeTodo} 
+           completeTodo={completeTodo}/>
         ))}
       </div>
       <TodoForm addAll={addAll}/>
